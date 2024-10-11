fedaf88640aacdbd55a477dc6e8d9324fd1d79bd
WankioM <tracywankio29@gmail.com>
2024-10-11 23:46:06 +0300

    lend

--- src/Components/Financing/LendDetails.tsx
+++ src/Components/Financing/LendDetails.tsx
@@ -6,7 +6,7 @@ const customStyles: CSSProperties = {
   ['--select-button-svg' as string]: "url('data:image/svg+xml...",
   fontFamily: '"Public Sans", "Noto Sans", sans-serif'
 };
-const BorrowDetails: React.FC = () => {
+const LendDetails: React.FC = () => {
   return (<div> Borrow details</div>)}
 
-export default BorrowDetails;
+export default LendDetails;
