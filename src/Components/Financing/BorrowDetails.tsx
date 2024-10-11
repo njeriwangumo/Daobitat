b964ea6079490f146d1e86adfe532d50a7fd21fc
WankioM <tracywankio29@gmail.com>
2024-10-11 23:45:05 +0300

    Financing pages

---
+++ src/Components/Financing/BorrowDetails.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { CSSProperties } from 'react';
+
+
+const customStyles: CSSProperties = {
+  ['--select-button-svg' as string]: "url('data:image/svg+xml...",
+  fontFamily: '"Public Sans", "Noto Sans", sans-serif'
+};
+const BorrowDetails: React.FC = () => {
+  return (<div> Borrow details</div>)}
+
+export default BorrowDetails;
