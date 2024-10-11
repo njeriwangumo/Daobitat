7d39b51bb445c16ac13ddcd552bdb13bbb8d62d8
WankioM <tracywankio29@gmail.com>
2024-10-12 00:06:36 +0300

    switching tabs

--- src/Components/Financing/Financing.tsx
+++ src/Components/Financing/Financing.tsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { CSSProperties } from 'react';
 import FinancingHeader from './FinancingHeader';
 import LoanDetails from './LoanDetails';
+import LendDetails from './LendDetails';
+import ApprovedRequests from './ApprovedRequests';
+import PendingRequests from './PendingRequests';
+import Statement from './Statement';
 
 
 const customStyles: CSSProperties = {
@@ -9,14 +13,32 @@ const customStyles: CSSProperties = {
   fontFamily: '"Public Sans", "Noto Sans", sans-serif'
 };
 const FinancingDashboard: React.FC = () => {
+  const [currentTab, setCurrentTab] = useState('Borrow');
+
+  const renderContent = () => {
+    switch (currentTab) {
+      case 'Borrow':
+        return <LoanDetails />;
+      case 'Lend':
+        return <LendDetails />;
+      case 'ApprovedRequests':
+        return <ApprovedRequests />;
+      case 'PendingRequests':
+        return <PendingRequests />;
+      case 'Statement':
+        return <Statement />;
+      default:
+        return <LoanDetails />;
+    }
+  };
+
   return (
     <div
       className="relative flex size-full min-h-screen flex-col bg-[#181114] dark group/design-root overflow-x-hidden"
       style={customStyles}
     >
-      <FinancingHeader />
-      <LoanDetails />
-      
+      <FinancingHeader currentTab={currentTab} onTabChange={setCurrentTab} />
+      {renderContent()}
     </div>
   );
 };
