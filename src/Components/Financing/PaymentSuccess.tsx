4004f4ae46a42e5bd7aadcc5be03d4953016bd07
njeriwangumo <njeriwangumo@gmail.com>
2024-10-13 23:55:15 +0300

    paymentsuccess

---
+++ src/Components/Financing/PaymentSuccess.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="container">
+      <h2>Your payment has been received successfully</h2>
+      <a href="https://storyset.com/online" target="_blank" rel="noopener noreferrer">
+        Online illustrations by Storyset
+      </a>
+    </div>
+  );
+};
+
+export default App;
